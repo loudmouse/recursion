c4f5f7f7413f35098621f6cc76cc34d8a54e2f3e
Nolan Neeley <nolanneeley@gmail.com>
2018-02-06 06:34:01 -0600

    added iterative fib method

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,14 +1,25 @@
-#Define a recursive function that takes an argument n and returns the fibonacci value of that position.
+#Define an iterative method that takes an argument n and returns the fibonacci value of that position
 #The fibonacci sequence is 0, 1, 1, 2, 3, 5, 8, 13, 21...
-#So fib(5) should return 5 and fib(6) should return 8.
 
-def fibonacci(n)
+
+def fibs(n)
+  fib = [0,1]
+  n.times {fib << fib[-1] + fib[-2]}
+  return fib[n]
+end
+puts fibs(5)
+puts fibs(6)
+
+
+#Define a recursive method that takes an argument n and returns the fibonacci value of that position.
+
+def fib_recursive(n)
   if n < 2
     n
   else
-    fibonacci(n-1) + fibonacci(n-2)
+    fib_recursive(n-1) + fib_recursive(n-2)
   end
 end
 
-puts fibonacci(5)
-puts fibonacci(6)
+puts fib_recursive(5)
+puts fib_recursive(6)
